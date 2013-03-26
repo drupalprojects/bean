e02a2d383810452cada18830589346d1e8dd0d7f
brantwynn <brant.wynn@acquia.com>
2013-03-26 00:08:54 -0500

    Updated CHANGELOG.txt and bean.api.php

--- bean.api.php
+++ bean.api.php
@@ -22,16 +22,21 @@ function hook_bean_types_api_info() {
  */
 function hook_bean_types() {
   $plugins = array();
-
   $plugins['plugin_key'] = array(
     'label' => t('Title'),
     'description' => t('Description'),
-    // This is optional.  Set it to TRUE if you do not want the plugin to be displayed in the UI
+    // This is optional.  Set it to TRUE if you do not want the plugin to be 
+    // displayed in the UI.
     'abstract' => FALSE,
     'handler' => array(
       'class' => 'ClassName',
       'parent' => 'bean',
     ),
+    // This should be pointing to the path of your custom bean plugin module.
+    'path' => drupal_get_path('module', 'example_bean') . '/plugins';
+    // Class files should be named accordingly in order to support ctools
+    // autoloading procedures.
+    'file' => 'TaxListingBean.class.php'
   );
 
   return $plugins;
